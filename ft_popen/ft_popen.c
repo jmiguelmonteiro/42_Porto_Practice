aa205c7c6353cfd1baf65c47d99a94b4082d7e24
Miguel Monteiro <mmonteiro@MacBook-Ana.local>
2025-11-14 09:50:02 +0000

    teste

--- ft_popen/ft_popen.c
+++ ft_popen/ft_popen.c
@@ -2,11 +2,57 @@
 
 int ft_popen(const char *file, char *const argv[], char type)
 {
+	pid_t	pid;
+	int		fd[2];
 
-	(void)file;
-	(void)argv;
-	(void)type;
+	if (!file || !argv || (type != 'r' && type != 'w'))
+		return (-1);
 
+	if (pipe(fd) == -1)
+		return (-1);
+	// fd[0] - read end
+	// fd[1] - write end
+
+	pid = fork();
+	if (pid == -1)
+	{
+		// Fork failed
+		close(fd[0]);
+		close(fd[1]);
+		return (-1);
+	}
+	if (pid == 0)
+	{
+		// Child process
+		if (type == 'r')
+		{
+			if (dup2(fd[1], STDOUT_FILENO) == -1)
+				exit(EXIT_FAILURE);
+		}
+		else
+		{
+			if (dup2(fd[0], STDIN_FILENO) == -1)
+				exit(EXIT_FAILURE);
+		}
+		close(fd[0]);
+		close(fd[1]);
+		execvp(file, argv);
+		exit(EXIT_FAILURE);
+	}
+	else if (pid > 0)
+	{
+		// Parent process
+		if (type == 'r')
+		{
+			close(fd[1]); // Close write end
+			return (fd[0]); // Return read end
+		}
+		else
+		{
+			close(fd[0]); // Close read end
+			return (fd[1]); // Return write end
+		}
+	}
 	return (-1);
 }
 
